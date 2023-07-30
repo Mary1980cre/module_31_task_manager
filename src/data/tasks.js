1cee2aad4531273b99ec4d04fca59e0c87840987
Mary1980cre <marianna.e@mail.ru>
2023-07-30 16:21:55 +0300

    commetr

---
+++ src/data/tasks.js
@@ -0,0 +1,10 @@
+export const tasks = [
+    {id: 1, userId: null, statusId: 11, status: 'backlog', text: 'Запись №1 – Вход в систему'}, 
+    {id: 2, userId: null, statusId: 11, status: 'backlog', text: 'Запись №2 - Проверка работоспособности'}, 
+    {id: 3, userId: null, statusId: 12, status: 'ready', text: 'Запись №3 – Пробный запуск-в работе'}, 
+    {id: 4, userId: null, statusId: 12, status: 'ready', text: 'Запись №4-Отладка- в работе'}, 
+    {id: 5, userId: null, statusId: 13, status: 'in_progress', text: 'Запись №5 – В работе '}, 
+    {id: 6, userId: null, statusId: 13, status: 'in_progress', text: 'Запись №6 - В работе'}, 
+    {id: 7, userId: null, statusId: 14, status: 'finished', text: 'Запись №7 - Отладка'}, 
+    {id: 8, userId: null, statusId: 14, status: 'finished', text: 'Запись №8 - Финиш'}, 
+];
